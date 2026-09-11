0cc658238dc493fa711741dac173c3f914fc83a5
Rohit Kumar <kumarrht87@gmail.com>
2026-09-11 17:39:15 +0530

    Add project cards styling

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%+(ac6qa8o2%)(2gm=b!teei-4t5i4rv#n2m8f745yx-lrxyrs'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
